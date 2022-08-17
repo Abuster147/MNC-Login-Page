39f54f3c0de8d3fae02138f7597cb852f71b7f95
akash kumar <akash123smr@gmail.com>
2022-08-17 23:02:28 +0530

    first

---
+++ script.js
@@ -0,0 +1,13 @@
+function validate(){
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if(username==akash && password==akash){
+    alert("login successful");
+    window.open("profile.html");
+    return false;
+  }else{
+    alert("login failed");
+  }
+
+}
